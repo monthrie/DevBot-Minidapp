ef1dbad1f7823cbf7ccd8dff035eb818c6f6188c
monthrie <110186937+monthrie@users.noreply.github.com>
2024-09-26 17:17:41 +0100

    css moved

--- experiments/broadcast/service.js
+++ experiments/broadcast/service.js
@@ -4,7 +4,6 @@ MDS.init(function(msg){
     } else if(msg.event == "MAXIMA"){
         // Handle incoming Maxima messages
         if(msg.data.application == "YourAppName"){
-            console.log("Received message:", msg.data);
             // Notify the main app
             MDS.notify("New message received from " + msg.data.from);
         }
